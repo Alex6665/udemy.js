f7145e27b6becbae6232cf03f54788e3677fbe5b
Alex6665 <alex6665stefan@yahoo.com>
2022-06-12 22:05:17 +0300

    fa

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,27 @@
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+
+console.log("Alex");
+console.log(23);
+
+let firstName = "Stefan";
+
+// let first = "Alex";
+// let firstNamePerson;
+// let first_name_person;
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let Alex_Stefan = "AS";
+let $new = 25;
+let name = "Alex";
+let PI = 3.1415;
+
+let myFirstJob = "Coder";
+let myCurrentJob = "Engineer";
+
+let job1 = "programmer";
+let job2 = "Engineer";
+console.log(myFirstJob);
